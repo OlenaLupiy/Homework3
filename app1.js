19c51ee38c521c1cbbb82c9fd8446407655632f5
OlenaLupiy <84191532+OlenaLupiy@users.noreply.github.com>
2021-06-01 11:55:25 +0300

    Add files via upload
    Done

--- app1.js
+++ app1.js
@@ -63,45 +63,67 @@ function findMin() {
 
 findMin(3, 17, 9, 0.55, -7);
 
-//Task 6
+// Task 6
 
-// function findUnique(...a, b, c) {      
-//      if (a!= b && b != c && a!=c ){
-//          console.log(true)}
-//      else console.log(false)};
-    
-// let arr2 = [2, 3, 2]
-// findUnique(...arr2);
+function findUnique(A)
+{   
+    let n = A.length;
+    for (let i = 0; i < n-1; i++)
+     { for (let j = i+1; j < n; j++)
+        { if (A[i] === A[j]) return false; }
+     }
+    return true;
+}
+console.log(findUnique([1, 2, 3]));
+console.log(findUnique([1, 2, 3, 1]))
 
-
-
-function findUnique1(...x){
-    for (let i =0; i < arr2.length; i++){
-        for (let j = (i+1); j< arr2.length; j++){
-        if (arr2[j] != arr2[j+1] && arr2[i] != arr2[i+1] && arr2[i] != arr2[j+1] && arr2[j] != arr2[i+1]){
-            console.log(true)
-        }
-        else{
-            console.log(false)
-        }
-    }
-}};
-let arr2=[1, 2, 3, 2]
-findUnique1 (...arr2);
-//for(let i = 0; i < arr.length; i++){
-    //for(let j = (i + 1); j < arr.length; j++){
-        
-
-
+// Не працює, а чому - не розумію
+// function findUnique1(arr){
+//     for (let i =0; i<arr.length-1; i++)
+//     {
+//         for (let j = i+1; j<arr.length; j++){
+//         if (arr[i] === arr[j])
+//             return false;
+//         }
+//         return true;
+//     }
+// };
+//  console.log(findUnique1([1,2,3,2]));
 // Task 7
-// let arr = [];
-// let count1;
-// function lastElem(a, b) {
-//     if ()
-    
-// }
+ let arrReturn = [1, 2, 3, 4, 5, 6];
+ function lastElem(arr)
+  {
+     return arrReturn[arrReturn.length - 1];
+    }
+    console.log(lastElem(arrReturn));
+
+// Task 7.1
+let array = [1,2,3,4,5,6,7,8,9];
+
+function getLastElem(array, n) {
+    if (array == null) return false;
+    if (n == null) return array[array.length - 1];    
+  return array.slice(Math.max(array.length - n, 0));
+};
+
+console.log(getLastElem(array));    // 9
+console.log(getLastElem(array, 4)); // 6,7,8,9
+console.log(getLastElem(array, 12));       // 1,2,3,4,5,6,7,8,9
+
 
 // Task 8
-function capitalLetters(...x) {
+
+
+
+function CapitalLetter(str){
+    let arr = str.split(' ');    
+  for(let i = 0; i< arr.length; i++) 
+  {
+     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+     let result = arr.join(' '); 
+     console.log(result);
+    }
     
-}
+CapitalLetter('i love java script'); 
+
